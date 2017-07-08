37839bb37643c92398f472c49a20ed8d086ea4ce
Brandon Messick <brandon.k.messick@gmail.com>
2017-07-08 11:29:38 -0400

    Had to rebuild PositionREport and OpenDoor
    rebuilt PositionREport and still working on completing OpenDoor C++

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,10 +1,10 @@
 // Copyright Cryptix Durden
 
-#include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "OpenDoor.h"
+
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,20 +15,17 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
-	UE_LOG(LogTemp, Error, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
-	
+	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
